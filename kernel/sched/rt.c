6b81069478fca3daaf885c9ebcd2710dec8663c4
Trevor Brandt <tjbrandt@gmail.com>
2012-07-03 15:11:35 -0700

    As an exercise, I went ahead and duplicated rt_sched_class and named it fair_sched_class. I got it to compile, but I the kernel crashed again in the same place. Kind of strange. I plan to reverse my changes and commit again, but wanted to save this state.

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -2006,6 +2006,41 @@ static unsigned int get_rr_interval_rt(struct rq *rq, struct task_struct *task)
 }
 
 const struct sched_class rt_sched_class = {
+//#if 0
+	.next			= &fair_sched_class,
+//#else
+	//.next			= &idle_sched_class,
+//#endif
+	.enqueue_task		= enqueue_task_rt,
+	.dequeue_task		= dequeue_task_rt,
+	.yield_task		= yield_task_rt,
+
+	.check_preempt_curr	= check_preempt_curr_rt,
+
+	.pick_next_task		= pick_next_task_rt,
+	.put_prev_task		= put_prev_task_rt,
+
+#ifdef CONFIG_SMP
+	.select_task_rq		= select_task_rq_rt,
+
+	.set_cpus_allowed       = set_cpus_allowed_rt,
+	.rq_online              = rq_online_rt,
+	.rq_offline             = rq_offline_rt,
+	.pre_schedule		= pre_schedule_rt,
+	.post_schedule		= post_schedule_rt,
+	.task_woken		= task_woken_rt,
+	.switched_from		= switched_from_rt,
+#endif
+
+	.set_curr_task          = set_curr_task_rt,
+	.task_tick		= task_tick_rt,
+
+	.get_rr_interval	= get_rr_interval_rt,
+
+	.prio_changed		= prio_changed_rt,
+	.switched_to		= switched_to_rt,
+};
+const struct sched_class fair_sched_class = {
 #if 0
 	.next			= &fair_sched_class,
 #else
